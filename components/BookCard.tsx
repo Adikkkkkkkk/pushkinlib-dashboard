e976673d95e5c43cca72c310bf2168e7d287a2f9
Adikkkkkkkk <raximov_adil@mail.ru>
2025-04-17 16:48:46 +0500

    Пофиксил переход на страницу книг которые рендерятся динамически с учетом мультиязычности

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useLocale } from 'next-intl';
 import Link from 'next/link';
 import React from 'react';
 import BookCover from '@/components/BookCover';
@@ -12,37 +15,41 @@ const BookCard = ({
   coverColor,
   coverUrl,
   isLoanedBook = false,
-}: Book) => (
-  <li className={cn(isLoanedBook && 'xs:w-52 w-full')}>
-    <Link
-      href={`/books/${id}`}
-      className={cn(isLoanedBook && 'w-full flex flex-col items-center')}
-    >
-      <BookCover coverColor={coverColor} coverImage={coverUrl} />
+}: Book) => {
+  const locale = useLocale();
 
-      <div className={cn('mt-4', !isLoanedBook && 'xs:w-40 max-w-28')}>
-        <p className="book-title">{title}</p>
-        <p className="book-genre">{genre}</p>
-      </div>
+  return (
+    <li className={cn(isLoanedBook && 'xs:w-52 w-full')}>
+      <Link
+        href={`/${locale}/books/${id}`}
+        className={cn(isLoanedBook && 'w-full flex flex-col items-center')}
+      >
+        <BookCover coverColor={coverColor} coverImage={coverUrl} />
 
-      {isLoanedBook && (
-        <div className="mt-3 w-full">
-          <div className="book-loaned">
-            <Image
-              src="/icons/calendar.svg"
-              alt="calendar"
-              width={18}
-              height={18}
-              className="object-contain"
-            />
-            <p className="text-light-100">11 дней до возврата книги</p>
-          </div>
-
-          <Button className="book-btn">Скачать чек</Button>
+        <div className={cn('mt-4', !isLoanedBook && 'xs:w-40 max-w-28')}>
+          <p className="book-title">{title}</p>
+          <p className="book-genre">{genre}</p>
         </div>
-      )}
-    </Link>
-  </li>
-);
+
+        {isLoanedBook && (
+          <div className="mt-3 w-full">
+            <div className="book-loaned">
+              <Image
+                src="/icons/calendar.svg"
+                alt="calendar"
+                width={18}
+                height={18}
+                className="object-contain"
+              />
+              <p className="text-light-100">11 дней до возврата книги</p>
+            </div>
+
+            <Button className="book-btn">Скачать чек</Button>
+          </div>
+        )}
+      </Link>
+    </li>
+  );
+};
 
 export default BookCard;
